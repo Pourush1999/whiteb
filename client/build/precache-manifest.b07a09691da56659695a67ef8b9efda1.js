69faf9d5a8fab72ea7deed25f4d89e296d734794
Pourush1999 <pourushkhanna@gmail.com>
2021-07-01 17:16:17 +0530

    first commit

---
+++ client/build/precache-manifest.b07a09691da56659695a67ef8b9efda1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88ecc8916c29c6a88e34806f9c188476",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a79b4fa5bf8f2a16e4c",
+    "url": "/static/css/main.afca1a56.chunk.css"
+  },
+  {
+    "revision": "aecb7f34388c9b903ce8",
+    "url": "/static/js/2.10692b4d.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.10692b4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a79b4fa5bf8f2a16e4c",
+    "url": "/static/js/main.28295742.chunk.js"
+  },
+  {
+    "revision": "5fb930173a324f639f17",
+    "url": "/static/js/runtime-main.f2043711.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
